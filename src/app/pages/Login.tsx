2ca663a4a8834668657bc88a1fcef30816f0aec1
Leonardo Branco Costa <leonardobrancocosta@eicschool.com.br>
2026-03-08 19:10:59 -0300

    fix: react-router-dom import in Login

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { mockUsers } from "../data/mockData";
 
