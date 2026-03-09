48d73a0b83132211e96fb314dc28142bf01bee5a
leobranco88 <133527996+leobranco88@users.noreply.github.com>
2026-03-08 22:34:22 -0300

    Update routes.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Login } from "./pages/Login";
+import Login from "./pages/Login";
 import { ProfessorDashboard } from "./pages/ProfessorDashboard";
 import { CreateReport } from "./pages/CreateReport";
 import { ViewReport } from "./pages/ViewReport";
