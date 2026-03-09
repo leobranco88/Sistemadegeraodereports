cd35352556ad36da7d245d302104a6fea98c5117
leobranco88 <133527996+leobranco88@users.noreply.github.com>
2026-03-09 00:05:42 -0300

    Update Login.tsx

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
 import logo from "../../imports/logo.svg";
